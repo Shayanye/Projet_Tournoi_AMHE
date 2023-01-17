60045e8737505de4bfd85181f30af47d9f0705be
valen <valen@Patoche>
2023-01-17 12:56:48 +0100

    Initial commit

---
+++ AMHE/src/main/java/fr/toulouse/miage/amhe/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.toulouse.miage.amhe;
+
+/**
+ *
+ * @author valen
+ */
+public class NewClass{
+ public int number(){
+return 2;
+    }
+}
