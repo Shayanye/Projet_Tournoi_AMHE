15ea652d8dcc275d6676fda61fb6ec277afe282d
charles <charles.caranton@gmail.com>
2023-02-17 14:36:07 +0100

    attribut journal dans tournoi + redefinition toString tournoi

--- src/main/java/fr/toulouse/miage/amhe/tournoi/Solo.java
+++ src/main/java/fr/toulouse/miage/amhe/tournoi/Solo.java
@@ -15,8 +15,7 @@ public class Solo extends Tournoi{
 	//private Duelliste duellistes;
 	private ArrayList<Duelliste> duellistes;
 	private ArrayList<MancheJoueur> manches;
-
-	private ArrayList<MancheJoueur> manches_effectuees;
+	private String journal;
 
 
 	
@@ -29,7 +28,7 @@ public class Solo extends Tournoi{
 		//il me faut un constructeur de Duelliste qui prend en paramètre un int (nbParticipant) et qui me créer un tableau de duelliste de cette taille.
 		this.duellistes = new ArrayList<>(this.getNbParticipant());
 		this.manches = new ArrayList<>(this.getNbParticipant()-1);
-		this.manches_effectuees  = new ArrayList<>();
+		this.journal = "";
 
 
 	}
@@ -78,24 +77,26 @@ public class Solo extends Tournoi{
 		return i;
 	}
 
+	public String toString(){
+		return this.journal;
+	}
+
 
 	public String jouerToutesLesManches(){
 		ArrayList<Duelliste> gagnants = new ArrayList<>();
-		String journal = "";
 		int tour_a_effectuer = this.calculTourAEffectuer();
 		int tour = 1;
 		int nbManches = 0;
 
 		for(int i = 0; i<tour_a_effectuer; i++) {
 			nbManches = this.manches.size();
-			journal += "\n\nTour " + tour + "\n";
+			this.journal += "\n\nTour " + tour + "\n";
 			tour++;
 			for (int j = 0; j < nbManches; j++) {
-				journal += this.manches.get(j).toString();
+				this.journal += this.manches.get(j).toString();
 				Duelliste joueurGagnant = this.manches.get(j).jouerManche();
 				gagnants.add(joueurGagnant);
-				journal += "le joueur " + joueurGagnant.getNom() + " a gagné sur un score de : " +this.manches.get(j).getScore1()+" a " +this.manches.get(j).getScore2()+"\n";
-				this.manches_effectuees.add(this.manches.get(j));
+				this.journal += "le joueur " + joueurGagnant.getNom() + " a gagné sur un score de : " +this.manches.get(j).getScore1()+" a " +this.manches.get(j).getScore2()+"\n";
 
 
 			}
@@ -116,13 +117,10 @@ public class Solo extends Tournoi{
 				journal += "\n*"+this.manches.get(it).toString()+"\n";
 			}*/
 		}
-		for (int it = 0; it < this.manches_effectuees.size(); it++) {
-			journal += "\n*"+this.manches_effectuees.get(it).toString()+"\n";
-		}
 
-		journal += "\n\n\nLe gagnant du tournoi : " +this.getNom()+ " est " +gagnants.get(0).getNom();
+		this.journal += "\n\n\nLe gagnant du tournoi : " +this.getNom()+ " est " +gagnants.get(0).getNom();
 
-		return journal;
+		return this.journal;
 
 	}
 
